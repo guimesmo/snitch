5e7b569443a69c2ed4a06778ea21e01e9fb5074c
Valder Gallo <valdergallo@gmail.com>
2014-03-29 09:22:56 -0300

    serve static files

--- urls.py
+++ urls.py
@@ -3,6 +3,5 @@ from twisted.web.resource import Resource
 
 root_url = Resource()
 
-root_url.putChild("foo", File("/tmp"))
-root_url.putChild("bar", File("/lost+found"))
-root_url.putChild("baz", File("/opt"))
+# serve static files
+root_url.putChild("static", File("/static"))
