c76a79422bb3689b70f919e7013400c86255741d
Valder Gallo <valdergallo@gmail.com>
2014-05-03 15:38:54 -0300

    fix user model

--- snitch/models.py
+++ snitch/models.py
@@ -11,14 +11,15 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    user_email = Column(String(50))
-    user_passwd = Column(String(50))
-    user_full_name = Column(String(50))
-    user_is_active = Column(Boolean(1))
+    email = Column(String(150))
+    password = Column(String(150))
+    nickname = Column(String(150))
+    fullname = Column(String(250))
+    is_active = Column(Boolean(1))
 
     def __repr__(self):
-        return "<User(name='%s', fullname='%s', password='%s')>" % (
-                         self.name, self.fullname, self.password)
+        return "<User(nickname='%s', fullname='%s', password='%s')>" % (
+                         self.nickname, self.fullname, self.password)
 
 
 class WebSite(Base):
